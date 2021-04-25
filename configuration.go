35dbc7f24ed6274cf40f0b23bc884fe58aa11ecc
Alessandro Duca <a.duca@trenolab.com>
2021-04-25 19:50:31 +0200

    removed parse method

--- configuration.go
+++ configuration.go
@@ -1,14 +1,8 @@
 package ubot
 
-import "encoding/json"
-
 type Configuration struct {
 	APIToken   string `json:"api_token"`
 	ServerPort string `json:"server_port"`
 	WebhookUrl string `json:"webhook_url"`
 	WorkerNo   int    `json:"worker_no"`
-}
-
-func (c *Configuration) Parse(data []byte) error {
-	return json.Unmarshal(data, c)
 }
